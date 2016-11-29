9ba0afc4282e1bd4a8817ea7d8ae80d045275769
Reactive XYZ Dev <dev@reactive.xyz>
2016-11-29 15:29:02 -0500

    Update
    - Added common definitions

--- distribution_code_reach/distribution_code_reach/distribution_code_reach.ino
+++ distribution_code_reach/distribution_code_reach/distribution_code_reach.ino
@@ -1,9 +1,67 @@
+#include <gamma.h>
+#include <RGBmatrixPanel.h>
+#include <Adafruit_GFX.h>
+#include <EEPROM.h>
+
+// define the wiring of the LED screen
+const uint8_t CLK  = 8;
+const uint8_t LAT = A3;
+const uint8_t OE = 9;
+const uint8_t A = A0;
+const uint8_t B = A1;
+const uint8_t C = A2;
+
+// define the wiring of the inputs
+const int POTENTIOMETER_PIN_NUMBER = 5;
+const int BUTTON_PIN_NUMBER = 10;
+const int RESTART_PIN_NUMBER = 11;
+
+// define the number of defenders in a game
+const int NUM_DEFENDERS = 4;
+
+// define the matrix that represents the LED screen
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
+
+// define Color classes and common colors
+class Color {
+  public:
+    int red;
+    int green;
+    int blue;
+    Color() {
+      red = 0;
+      green = 0;
+      blue = 0;
+    }
+    Color(int r, int g, int b) {
+      red = r;
+      green = g;
+      blue = b;
+    }
+    uint16_t to_333() const {
+      return matrix.Color333(red, green, blue);
+    }
+};
+
+const Color BLACK(0, 0, 0);
+const Color RED(7, 0, 0);
+const Color ORANGE(7, 4, 0);
+const Color YELLOW(7, 7, 0);
+const Color GREEN(0, 7, 0);
+const Color BLUE(0, 0, 7);
+const Color PURPLE(7, 0, 7);
+const Color WHITE(7, 7, 7);
+const Color LIME(4, 7, 0);
+const Color AQUA(0, 7, 7);
+
+// Arduino's native setup function
+// runs only once
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
+// Arduino's native loop function
+// runs millions of times per second
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
